974d37e9f7ceceb1edffb873fdbbf9a9776cf76a
rdv25772 <afz_git@rdv25772.nl>
2022-01-20 11:20:10 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
@@ -10,7 +11,7 @@ export class ProductService {
 
   private _albumUrl: string = '../assets/album.json';
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map(response => response.json());
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map(response => <Album>response.json());
   }
 }
